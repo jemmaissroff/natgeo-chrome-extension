1ad9c8b75f3d662b92e492cb3ca3109b863da0e7
Jemma Issroff <jemmaissroff@gmail.com>
2020-09-25 11:50:47 -0400

    Works on setup now too

--- photo-of-day.js
+++ photo-of-day.js
@@ -1,9 +1,9 @@
 chrome.storage.sync.get('date', function(result) {
 	// Checks if date is unset, or the previous day, in which case will
 	// pull the image source from nat geo
-	if (!result['date'] || result['date'] != (new Date().toISOString().slice(0, 10))) {
+	if (result['date'] != (new Date().toISOString().slice(0, 10))) {
 		var date = new Date().toISOString().slice(0, 10)
-		chrome.storage.sync.set({ 'date' : date }, function() {});
+		chrome.storage.sync.set({ 'date' : date }, function() {})
 
 		fetch('https://www.nationalgeographic.com/photography/photo-of-the-day/')
 			.then(
@@ -20,6 +20,7 @@ chrome.storage.sync.get('date', function(result) {
 				function(text) {
 					var doc = new DOMParser().parseFromString(text, "text/html")
 					var selected = doc.querySelector("meta[property='og:image']").getAttribute("content")
+					document.querySelector(".photo-of-day").setAttribute("src", selected);
 					chrome.storage.sync.set({ 'img-src' : selected }, function() {});
 
 				}
