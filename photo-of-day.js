ef8c0ff3e0c52d4b90583df3c3b7f4cd915fb7c9
Jemma Issroff <jemmaissroff@gmail.com>
2020-09-25 10:57:25 -0400

    Caches image result

--- photo-of-day.js
+++ photo-of-day.js
@@ -1,20 +1,34 @@
-fetch('https://www.nationalgeographic.com/photography/photo-of-the-day/')
-.then(
-    function(response) {
-	    if (response.status !== 200) {
-		    console.log('Looks like there was a problem. Status Code: ' +
-			    response.status);
-		    return Promise.reject("Failed to scrape photo");
-	    }
+chrome.storage.sync.get('date', function(result) {
+	// Checks if date is unset, or the previous day, in which case will
+	// pull the image source from nat geo
+	if (!result['date'] || result['date'] != (new Date().toISOString().slice(0, 10))) {
+		var date = new Date().toISOString().slice(0, 10)
+		chrome.storage.sync.set({ 'date' : date }, function() {});
 
-	    return response.text()
-    }
-).then(
-    function(text) {
-	    var doc = new DOMParser().parseFromString(text, "text/html")
-	    var selected = doc.querySelector("meta[property='og:image']").getAttribute("content")
-	    document.querySelector(".photo-of-day").setAttribute("src", selected)
-    }
-).catch(function(err) {
-	console.log('Fetch Error :-S', err);
+		fetch('https://www.nationalgeographic.com/photography/photo-of-the-day/')
+			.then(
+				function(response) {
+					if (response.status !== 200) {
+						console.log('Looks like there was a problem. Status Code: ' +
+							response.status);
+						return Promise.reject("Failed to scrape photo");
+					}
+
+					return response.text()
+				}
+			).then(
+				function(text) {
+					var doc = new DOMParser().parseFromString(text, "text/html")
+					var selected = doc.querySelector("meta[property='og:image']").getAttribute("content")
+					chrome.storage.sync.set({ 'img-src' : selected }, function() {});
+
+				}
+			).catch(function(err) {
+				console.log('Fetch Error :-S', err);
+			});
+	}
+
+	chrome.storage.sync.get('img-src', function(result) {
+		document.querySelector(".photo-of-day").setAttribute("src", result['img-src']);
+	});
 });
